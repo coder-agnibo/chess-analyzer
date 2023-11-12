878c14f6b031734cb7f5458fc96e3676af39bc55
Sohan <ronnie.basak96@gmail.com>
2023-11-12 23:32:00 +0530

    Working chessboard

--- src/views/Chessboard.tsx
+++ src/views/Chessboard.tsx
@@ -1,22 +1,16 @@
 import { useState } from 'react';
-import { ChessBoard } from 'chessboardjs';
+import Chessboard from 'chessboardjsx';
 
 
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    const board = ChessBoard('board1', {
-      draggable: true,
-      dropOffBoard: 'trash',
-      sparePieces: true
-    });
-  }, []);
+
 
   return (
     <>
       <div>
-        <div id="board1" style={{ width: 400 }}></div>
+        <Chessboard position="start" />
       </div>
     </>
   )
